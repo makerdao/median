84fe44cc7b8a100442d676bde54575084a4042f5
Mariano Conti <nanexcool@gmail.com>
2019-11-12 16:00:47 -0300

    Updates

--- src/median.sol
+++ src/median.sol
@@ -1,12 +1,44 @@
-pragma solidity ^0.5.2;
+pragma solidity >=0.5.10;
 
-contract Median {
+contract LibNote {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  usr,
+        bytes32  indexed  arg1,
+        bytes32  indexed  arg2,
+        bytes             data
+    ) anonymous;
+
+    modifier note {
+        _;
+        assembly {
+            // log an 'anonymous' event with a constant 6 words of calldata
+            // and four indexed topics: selector, caller, arg1 and arg2
+            let mark := msize                         // end of memory ensures zero
+            mstore(0x40, add(mark, 288))              // update free memory pointer
+            mstore(mark, 0x20)                        // bytes type data offset
+            mstore(add(mark, 0x20), 224)              // bytes size (padded)
+            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
+            log4(mark, 288,                           // calldata
+                 shl(224, shr(224, calldataload(0))), // msg.sig
+                 caller,                              // msg.sender
+                 calldataload(4),                     // arg1
+                 calldataload(36)                     // arg2
+                )
+        }
+    }
+}
+
+contract Median is LibNote {
 
     // --- Auth ---
-    mapping (address => uint256) public wards;
-    function rely(address guy) public auth { wards[guy] = 1; }
-    function deny(address guy) public auth { wards[guy] = 0; }
-    modifier auth { require(wards[msg.sender] == 1); _; }
+    mapping (address => uint) public wards;
+    function rely(address usr) external note auth { wards[usr] = 1; }
+    function deny(address usr) external note auth { wards[usr] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "Median/not-authorized");
+        _;
+    }
 
     uint128        val;
     uint32  public age;
@@ -22,7 +54,7 @@ contract Median {
     // Mapping for at most 256 oracles
     mapping (uint8 => address) public slot;
 
-    modifier toll { require(bud[msg.sender] == 1, "Contract is not whitelisted"); _;}
+    modifier toll { require(bud[msg.sender] == 1, "Median/contract-not-whitelisted"); _;}
 
     event LogMedianPrice(uint256 val, uint256 age);
 
@@ -32,7 +64,7 @@ contract Median {
     }
 
     function read() external view toll returns (uint256) {
-        require(val > 0, "Invalid price feed");
+        require(val > 0, "Median/invalid-price-feed");
         return val;
     }
 
@@ -51,7 +83,7 @@ contract Median {
         uint256[] calldata val_, uint256[] calldata age_,
         uint8[] calldata v, bytes32[] calldata r, bytes32[] calldata s) external
     {
-        require(val_.length == bar, "Not enough signed messages");
+        require(val_.length == bar, "Median/bar-too-low");
 
         uint256 bloom = 0;
         uint256 last = 0;
@@ -61,15 +93,15 @@ contract Median {
             // Validate the values were signed by an authorized oracle
             address signer = recover(val_[i], age_[i], v[i], r[i], s[i]);
             // Check that signer is an oracle
-            require(orcl[signer] == 1, "Signature by invalid oracle");
+            require(orcl[signer] == 1, "Median/invalid-oracle");
             // Price feed age greater than last medianizer age
-            require(age_[i] > zzz, "Stale message");
+            require(age_[i] > zzz, "Median/stale-message");
             // Check for ordered values
-            require(val_[i] >= last, "Messages not in order");
+            require(val_[i] >= last, "Median/messages-not-in-order");
             last = val_[i];
             // Bloom filter for signer uniqueness
             uint8 sl = uint8(uint256(signer) >> 152);
-            require((bloom >> sl) % 2 == 0, "Oracle already signed");
+            require((bloom >> sl) % 2 == 0, "Median/oracle-already-signed");
             bloom += uint256(2) ** sl;
         }
 
@@ -79,37 +111,37 @@ contract Median {
         emit LogMedianPrice(val, age);
     }
 
-    function lift(address[] calldata a) external auth {
+    function lift(address[] calldata a) external note auth {
         for (uint i = 0; i < a.length; i++) {
-            require(a[i] != address(0), "No oracle 0");
+            require(a[i] != address(0), "Median/no-oracle-0");
             uint8 s = uint8(uint256(a[i]) >> 152);
-            require(slot[s] == address(0), "Signer already exists");
+            require(slot[s] == address(0), "Median/signer-already-exists");
             orcl[a[i]] = 1;
             slot[s] = a[i];
         }
     }
 
-    function drop(address[] calldata a) external auth {
+    function drop(address[] calldata a) external note auth {
        for (uint i = 0; i < a.length; i++) {
             orcl[a[i]] = 0;
             slot[uint8(uint256(a[i]) >> 152)] = address(0);
        }
     }
 
-    function setBar(uint256 bar_) external auth {
-        require(bar_ > 0, "Quorum has to be greater than 0");
-        require(bar_ % 2 != 0, "Quorum has to be an odd number");
+    function setBar(uint256 bar_) external note auth {
+        require(bar_ > 0, "Median/quorum-is-zero");
+        require(bar_ % 2 != 0, "Median/quorum-not-odd-number");
         bar = bar_;
     }
 
-    function kiss(address[] calldata a) external auth {
+    function kiss(address[] calldata a) external note auth {
         for(uint i = 0; i < a.length; i++) {
-            require(a[i] != address(0), "No contract 0");
+            require(a[i] != address(0), "Median/no-contract-0");
             bud[a[i]] = 1;
         }
     }
 
-    function diss(address[] calldata a) external auth {
+    function diss(address[] calldata a) external note auth {
         for(uint i = 0; i < a.length; i++) {
             bud[a[i]] = 0;
         }
